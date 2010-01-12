d6cbd31b4eb2026218a1e0402344d651082d6711
Kunio Murasawa <kunio.murasawa@gmail.com>
2010-01-12 17:56:46 +0900

    removed semicolons

--- src/cmd/twgo.go
+++ src/cmd/twgo.go
@@ -6,166 +6,166 @@
 package main
 
 import (
-	"twitter";
-	"os";
-	"flag";
-	"fmt";
-	"io/ioutil";
-	"regexp";
+	"twitter"
+	"os"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"regexp"
 )
 
 func main() {
-	var err os.Error;
-	var statuses []twitter.Status;
-	var lists []twitter.List;
-	var acc account;
-	var user twitter.User;
-	var users []twitter.User;
-	var rate twitter.Rate;
-	var status twitter.Status;
+	var err os.Error
+	var statuses []twitter.Status
+	var lists []twitter.List
+	var acc account
+	var user twitter.User
+	var users []twitter.User
+	var rate twitter.Rate
+	var status twitter.Status
 
-	flag.Parse();
+	flag.Parse()
 
 	// 設定ファイルから
 	if acc, err = readConfFile(); err != nil {
-		os.Stderr.WriteString(err.String() + "\n");
-        os.Exit(1);
+		os.Stderr.WriteString(err.String() + "\n")
+        os.Exit(1)
 	}
-	tw := twitter.NewTwitter(acc.user, acc.password, false);
+	tw := twitter.NewTwitter(acc.user, acc.password, false)
 
 	switch flag.Arg(0) {
 	case "show":
 		if status, err = tw.Show(flag.Arg(1)); err == nil {
-			showStatus(status);
+			showStatus(status)
 		}
 	case "update":
-		err = tw.Update(flag.Arg(1));
+		err = tw.Update(flag.Arg(1))
 	case "destroy":
-		err = tw.Destroy(flag.Arg(1));
+		err = tw.Destroy(flag.Arg(1))
 	case "friends":
 		if statuses, err = tw.FriendsTimeline(nil); err == nil {
-			showTimeline(statuses);
+			showTimeline(statuses)
 		}
 	case "home":
 		if statuses, err = tw.HomeTimeline(nil); err == nil {
-			showTimeline(statuses);
+			showTimeline(statuses)
 		}
 	case "user":
-		opts := map[string]uint{twitter.OPTION_UserTimeline_Count: 5};
+		opts := map[string]uint{twitter.OPTION_UserTimeline_Count: 5}
 		if statuses, err = tw.UserTimeline(opts); err == nil {
-			showTimeline(statuses);
+			showTimeline(statuses)
 		}
 	case "mentions":
 		if statuses, err = tw.Mentions(nil); err == nil {
-			showTimeline(statuses);
+			showTimeline(statuses)
 		}
 	case "public":
 		if statuses, err = tw.PublicTimeline(); err == nil {
-			showTimeline(statuses);
+			showTimeline(statuses)
 		}
 	case "lists":
 		if lists, err = tw.GetLists(flag.Arg(1), nil); err == nil {
-			showLists(lists);
+			showLists(lists)
 		}
 	case "list":
 		if statuses, err = tw.ListStatuses(flag.Arg(1), flag.Arg(2), nil); err == nil {
-			showTimeline(statuses);
+			showTimeline(statuses)
 		}
 	case "my":
-		user, err = tw.VerifyCredentials();
+		user, err = tw.VerifyCredentials()
 		if err == nil {
-			showUser(user);
+			showUser(user)
 		}
 	case "rate":
-		rate, err = tw.RateLimitStatus(false);
+		rate, err = tw.RateLimitStatus(false)
 		if err == nil {
-			showRate(rate);
+			showRate(rate)
 		}
 	case "search":
 		if users, err = tw.UsersSearch(flag.Arg(1), nil); err == nil {
-			showUsers(users);
+			showUsers(users)
 		}
 	default:
 		err = os.ErrorString("Not supported")
 	}
 
 	if err != nil {
-		os.Stderr.WriteString(err.String() + "\n");
-        os.Exit(1);
+		os.Stderr.WriteString(err.String() + "\n")
+        os.Exit(1)
 	}
 
-	os.Exit(0);
+	os.Exit(0)
 }
 
 type account struct {
-	user string;
-	password string;
+	user string
+	password string
 }
 func readConfFile() (acc account, err os.Error) {
-	var buf []byte;
+	var buf []byte
 
-	path := os.Getenv("HOME") + "/.twgo.conf";	// "~/.twgo.conf" だとオープン出来ないので
+	path := os.Getenv("HOME") + "/.twgo.conf"	// "~/.twgo.conf" だとオープン出来ないので
 
 	if buf, err = ioutil.ReadFile(path); err == nil {
-		u, _ := regexp.Compile("USER=\"([^\"]+)\"");
-		p, _ := regexp.Compile("PASSWORD=\"([^\"]+)\"");
+		u, _ := regexp.Compile("USER=\"([^\"]+)\"")
+		p, _ := regexp.Compile("PASSWORD=\"([^\"]+)\"")
 		if users := u.MatchStrings(string(buf)); users != nil {
-			acc.user = users[1];
+			acc.user = users[1]
 		}
 		if passs := p.MatchStrings(string(buf)); passs != nil {
-			acc.password = passs[1];
+			acc.password = passs[1]
 		}
 	}
 
-	return;
+	return
 }
 
 func showTimeline(statuses []twitter.Status) {
 	for _, s := range statuses {
-		fmt.Println(s.Id, s.Text, s.Created_At, s.Source, s.User.Id);
+		fmt.Println(s.Id, s.Text, s.Created_At, s.Source, s.User.Id)
 	}
 }
 
 func showUser(u twitter.User) {
-	fmt.Printf("Id: %d\n", u.Id);
-	fmt.Printf("Name: %s\n", u.Name);
-	fmt.Printf("ScreenName: %s\n", u.Screen_Name);
-	fmt.Printf("Location: %s\n", u.Location);
-	fmt.Printf("Description: %s\n", u.Description);
-	fmt.Printf("ProfileImageUrl: %s\n", u.Profile_Image_Url);
-	fmt.Printf("Url: %s\n", u.Url);
-	fmt.Printf("Protected: %t\n", u.Protected);
-	fmt.Printf("FollowersCount: %d\n", u.Followers_Count);
-	fmt.Printf("FriendsCount: %d\n", u.Friends_Count);
-	fmt.Printf("FavouritesCount: %d\n", u.Favourites_Count);
-	fmt.Printf("UtcOffset: %d\n", u.Utc_Offset);
-	fmt.Printf("TimeZone: %s\n", u.Time_Zone);
-	fmt.Printf("StatusesCount: %d\n", u.Statuses_Count);
+	fmt.Printf("Id: %d\n", u.Id)
+	fmt.Printf("Name: %s\n", u.Name)
+	fmt.Printf("ScreenName: %s\n", u.Screen_Name)
+	fmt.Printf("Location: %s\n", u.Location)
+	fmt.Printf("Description: %s\n", u.Description)
+	fmt.Printf("ProfileImageUrl: %s\n", u.Profile_Image_Url)
+	fmt.Printf("Url: %s\n", u.Url)
+	fmt.Printf("Protected: %t\n", u.Protected)
+	fmt.Printf("FollowersCount: %d\n", u.Followers_Count)
+	fmt.Printf("FriendsCount: %d\n", u.Friends_Count)
+	fmt.Printf("FavouritesCount: %d\n", u.Favourites_Count)
+	fmt.Printf("UtcOffset: %d\n", u.Utc_Offset)
+	fmt.Printf("TimeZone: %s\n", u.Time_Zone)
+	fmt.Printf("StatusesCount: %d\n", u.Statuses_Count)
 }
 
 func showRate(r twitter.Rate) {
-	fmt.Printf("RemainingHits: %d\n", r.Remaining_Hits);
-	fmt.Printf("HourlyLimit: %d\n", r.Hourly_Limit);
-	fmt.Printf("ResetTime: %s\n", r.Reset_Time);
-	fmt.Printf("ResetTimeInSeconds: %d\n", r.Reset_Time_In_Seconds);
+	fmt.Printf("RemainingHits: %d\n", r.Remaining_Hits)
+	fmt.Printf("HourlyLimit: %d\n", r.Hourly_Limit)
+	fmt.Printf("ResetTime: %s\n", r.Reset_Time)
+	fmt.Printf("ResetTimeInSeconds: %d\n", r.Reset_Time_In_Seconds)
 }
 
 func showStatus(s twitter.Status) {
-	fmt.Printf("Id: %d\n", s.Id);
-	fmt.Printf("Text: %s\n", s.Text);
-	fmt.Printf("CreatedAt: %s\n", s.Created_At);
-	fmt.Printf("Source: %s\n", s.Source);
-	fmt.Printf("UserId: %d\n", s.User.Id);
+	fmt.Printf("Id: %d\n", s.Id)
+	fmt.Printf("Text: %s\n", s.Text)
+	fmt.Printf("CreatedAt: %s\n", s.Created_At)
+	fmt.Printf("Source: %s\n", s.Source)
+	fmt.Printf("UserId: %d\n", s.User.Id)
 }
 
 func showLists(lists []twitter.List) {
 	for _, l := range lists {
-		fmt.Println(l.Id, l.Name, l.Full_Name, l.Slug, l.Description, l.Member_Count, l.Uri, l.Mode, l.User.Id);
+		fmt.Println(l.Id, l.Name, l.Full_Name, l.Slug, l.Description, l.Member_Count, l.Uri, l.Mode, l.User.Id)
 	}
 }
 
 func showUsers(users []twitter.User) {
 	for _, u := range users {
-		fmt.Println(u.Id, u.Name, u.Screen_Name);
+		fmt.Println(u.Id, u.Name, u.Screen_Name)
 	}
 }
--- src/pkg/twitter.go
+++ src/pkg/twitter.go
@@ -8,502 +8,502 @@
 package twitter
 
 import (
-	"os";
-	"net";
-	"http";
-	"io";
-	"io/ioutil";
-	"bufio";
-	"strconv";
-	"strings";
-	"fmt";
-	"bytes";
-	"json";
-	"regexp";
-	"encoding/base64";
+	"os"
+	"net"
+	"http"
+	"io"
+	"io/ioutil"
+	"bufio"
+	"strconv"
+	"strings"
+	"fmt"
+	"bytes"
+	"json"
+	"regexp"
+	"encoding/base64"
 )
 
 // ユーザ情報
 type User struct {
-	Id uint64;
-	Name string;
-	Screen_Name string;
-	Location string;
-	Description string;
-	Profile_Image_Url string;
-	Url string;
-	Protected bool;
-	Followers_Count uint;
-	Friends_Count uint;
-	Favourites_Count uint;
-	Utc_Offset int;
-	Time_Zone string;
-	Statuses_Count uint;
+	Id uint64
+	Name string
+	Screen_Name string
+	Location string
+	Description string
+	Profile_Image_Url string
+	Url string
+	Protected bool
+	Followers_Count uint
+	Friends_Count uint
+	Favourites_Count uint
+	Utc_Offset int
+	Time_Zone string
+	Statuses_Count uint
 }
 
 // ステータス情報
 type Status struct {
-	Created_At string;
-	Id uint64;
-	Text string;
-	Source string;
-	User User;
+	Created_At string
+	Id uint64
+	Text string
+	Source string
+	User User
 }
 
 // List情報
 type List struct {
-	Id uint64;
-	Name string;
-	Full_Name string;
-	Slug string;
-	Description string;
-	Member_Count uint;
-	Uri string;
-	Mode bool;
-	User User;
+	Id uint64
+	Name string
+	Full_Name string
+	Slug string
+	Description string
+	Member_Count uint
+	Uri string
+	Mode bool
+	User User
 }
 
 // Twitter情報
 type Twitter struct {
-	Username string;
-	Password string;
-	useSsl bool;
+	Username string
+	Password string
+	useSsl bool
 }
 // コンストラクタ
 func NewTwitter(user, pass string, useSsl bool) *Twitter {
-	return &Twitter{user, pass, useSsl};
+	return &Twitter{user, pass, useSsl}
 }
 
 // verify credentials（自分のユーザ情報を取得したい時など）
 func (self *Twitter) VerifyCredentials() (user User, err os.Error) {
-	const path = "/account/verify_credentials.json";
+	const path = "/account/verify_credentials.json"
 
-	res, err := request(GET, HOST, path, "", self.Username, self.Password, self.useSsl);
+	res, err := request(GET, HOST, path, "", self.Username, self.Password, self.useSsl)
 	if res.StatusCode != 200 {
-		err = os.ErrorString(res.Status);
-		return;
+		err = os.ErrorString(res.Status)
+		return
 	}
 
 	// response body
 	if body, err := ioutil.ReadAll(res.Body); err == nil {
 		// user
-		var u User;
+		var u User
 		if ok, errtok := json.Unmarshal(string(body), &u); ok == true {
-			user = u;
+			user = u
 		} else {
-			err = os.ErrorString(errtok);
+			err = os.ErrorString(errtok)
 		}
 	}
-	res.Body.Close();
+	res.Body.Close()
 
-	return;
+	return
 }
 
 // レート情報
 type Rate struct {
-	Remaining_Hits uint;
-	Hourly_Limit uint;
-	Reset_Time string;
-	Reset_Time_In_Seconds uint;
+	Remaining_Hits uint
+	Hourly_Limit uint
+	Reset_Time string
+	Reset_Time_In_Seconds uint
 }
 // rate limit status
 func (self *Twitter) RateLimitStatus(ipRate bool) (rate Rate, err os.Error) {
-	const path = "/account/rate_limit_status.json";
-	var user, pass string;
+	const path = "/account/rate_limit_status.json"
+	var user, pass string
 
 	if ipRate == true {
 		// IP's rate
-		user = "";
-		pass = "";
+		user = ""
+		pass = ""
 	} else {
 		// User's rate
-		user = self.Username;
-		pass = self.Password;
+		user = self.Username
+		pass = self.Password
 	}
 
-	res, err := request(GET, HOST, path, "", user, pass, self.useSsl);
+	res, err := request(GET, HOST, path, "", user, pass, self.useSsl)
 	if res.StatusCode != 200 {
-		err = os.ErrorString(res.Status);
-		return;
+		err = os.ErrorString(res.Status)
+		return
 	}
 
 	// response body
 	if body, err := ioutil.ReadAll(res.Body); err == nil {
 		// rate limit
-		var r Rate;
+		var r Rate
 		if ok, errtok := json.Unmarshal(string(body), &r); ok == true {
-			rate = r;
+			rate = r
 		} else {
-			err = os.ErrorString(errtok);
+			err = os.ErrorString(errtok)
 		}
 	}
-	res.Body.Close();
+	res.Body.Close()
 
-	return;
+	return
 }
 
 // statuses show
 func (self *Twitter) Show(id string) (status Status, err os.Error) {
-	path := fmt.Sprintf("/statuses/show/%s.json", id);
+	path := fmt.Sprintf("/statuses/show/%s.json", id)
 
-	res, err := request(GET, HOST, path, "", self.Username, self.Password, self.useSsl);
+	res, err := request(GET, HOST, path, "", self.Username, self.Password, self.useSsl)
 	if res.StatusCode != 200 {
-		err = os.ErrorString(res.Status);
-		return;
+		err = os.ErrorString(res.Status)
+		return
 	}
 
 	// response body
 	if body, err := ioutil.ReadAll(res.Body); err == nil {
 		// status
-		var s Status;
+		var s Status
 		if ok, errtok := json.Unmarshal(string(body), &s); ok == true {
 			// source
-			re, _ := regexp.Compile("<a[^>]*>(.*)</a>");
+			re, _ := regexp.Compile("<a[^>]*>(.*)</a>")
 			if srcs := re.MatchStrings(s.Source); len(srcs) == 2 {
-				s.Source = srcs[1];
+				s.Source = srcs[1]
 			}
 
-			status = s;
+			status = s
 		} else {
-			err = os.ErrorString(errtok);
+			err = os.ErrorString(errtok)
 		}
 	}
-	res.Body.Close();
+	res.Body.Close()
 
-	return;
+	return
 }
 
 // statuses update（つぶやき）
 func (self *Twitter) Update(message string) (err os.Error) {
 	const (
-		path = "/statuses/update.json";
-		param = "status=";
+		path = "/statuses/update.json"
+		param = "status="
 	)
 
 	// body
-	body := param + http.URLEscape(message);
+	body := param + http.URLEscape(message)
 
-	res, err := request(POST, HOST, path, body, self.Username, self.Password, self.useSsl);
+	res, err := request(POST, HOST, path, body, self.Username, self.Password, self.useSsl)
 	if res.StatusCode != 200 {
-		err = os.ErrorString(res.Status);
-		return;
+		err = os.ErrorString(res.Status)
+		return
 	}
 
-	res.Body.Close();
+	res.Body.Close()
 
-	return;
+	return
 }
 
 // statuses destroy
 func (self *Twitter) Destroy(id string) (err os.Error) {
-	path := fmt.Sprintf("/statuses/destroy/%s.json", id);
+	path := fmt.Sprintf("/statuses/destroy/%s.json", id)
 
-	res, err := request(DELETE, HOST, path, "", self.Username, self.Password, self.useSsl);
+	res, err := request(DELETE, HOST, path, "", self.Username, self.Password, self.useSsl)
 	if res.StatusCode != 200 {
-		err = os.ErrorString(res.Status);
-		return;
+		err = os.ErrorString(res.Status)
+		return
 	}
 
-	res.Body.Close();
+	res.Body.Close()
 
-	return;
+	return
 }
 
 // statuses friends_timeline
 func (self *Twitter) PublicTimeline() (statuses []Status, err os.Error) {
-	const path = "/statuses/public_timeline.json";
+	const path = "/statuses/public_timeline.json"
 
-	return self.timeline(path, nil, "", "", self.useSsl);
+	return self.timeline(path, nil, "", "", self.useSsl)
 }
 
 // HomeTimelineの引数マップキーに使用
 const (
-	OPTION_HomeTimeline_SinceId = "?since_id=";
-	OPTION_HomeTimeline_MaxId = "?max_id=";
-	OPTION_HomeTimeline_Count = "?count=";
-	OPTION_HomeTimeline_Page = "?page=";
+	OPTION_HomeTimeline_SinceId = "?since_id="
+	OPTION_HomeTimeline_MaxId = "?max_id="
+	OPTION_HomeTimeline_Count = "?count="
+	OPTION_HomeTimeline_Page = "?page="
 )
 // statuses home_timeline
 func (self *Twitter) HomeTimeline(options map[string] uint) (statuses []Status, err os.Error) {
-	const path = "/statuses/home_timeline.json";
+	const path = "/statuses/home_timeline.json"
 
-	return self.timeline(path, options, self.Username, self.Password, self.useSsl);
+	return self.timeline(path, options, self.Username, self.Password, self.useSsl)
 }
 
 // FiendsTimelineの引数マップキーに使用
 const (
-	OPTION_FriendsTimeline_SinceId = "?since_id=";
-	OPTION_FriendsTimeline_MaxId = "?max_id=";
-	OPTION_FriendsTimeline_Count = "?count=";
-	OPTION_FriendsTimeline_Page = "?page=";
+	OPTION_FriendsTimeline_SinceId = "?since_id="
+	OPTION_FriendsTimeline_MaxId = "?max_id="
+	OPTION_FriendsTimeline_Count = "?count="
+	OPTION_FriendsTimeline_Page = "?page="
 )
 // statuses friends_timeline
 func (self *Twitter) FriendsTimeline(options map[string] uint) (statuses []Status, err os.Error) {
-	const path = "/statuses/friends_timeline.json";
+	const path = "/statuses/friends_timeline.json"
 
-	return self.timeline(path, options, self.Username, self.Password, self.useSsl);
+	return self.timeline(path, options, self.Username, self.Password, self.useSsl)
 }
 
 // UserTimelineの引数マップキーに使用
 const (
-	OPTION_UserTimeline_UserId = "?user_id=";
-	OPTION_UserTimeline_ScreenName = "?screen_name=";
-	OPTION_UserTimeline_SinceId = "?since_id=";
-	OPTION_UserTimeline_MaxId = "?max_id=";
-	OPTION_UserTimeline_Count = "?count=";
-	OPTION_UserTimeline_Page = "?page=";
+	OPTION_UserTimeline_UserId = "?user_id="
+	OPTION_UserTimeline_ScreenName = "?screen_name="
+	OPTION_UserTimeline_SinceId = "?since_id="
+	OPTION_UserTimeline_MaxId = "?max_id="
+	OPTION_UserTimeline_Count = "?count="
+	OPTION_UserTimeline_Page = "?page="
 )
 // statuses user_timeline
 func (self *Twitter) UserTimeline(options map[string] uint) (statuses []Status, err os.Error) {
-	const path = "/statuses/user_timeline.json";
+	const path = "/statuses/user_timeline.json"
 
-	return self.timeline(path, options, self.Username, self.Password, self.useSsl);
+	return self.timeline(path, options, self.Username, self.Password, self.useSsl)
 }
 
 // Mentionsの引数マップキーに使用
 const (
-	OPTION_Mentions_SinceId = "?since_id=";
-	OPTION_Mentions_MaxId = "?max_id=";
-	OPTION_Mentions_Count = "?count=";
-	OPTION_Mentions_Page = "?page=";
+	OPTION_Mentions_SinceId = "?since_id="
+	OPTION_Mentions_MaxId = "?max_id="
+	OPTION_Mentions_Count = "?count="
+	OPTION_Mentions_Page = "?page="
 )
 // statuses mentions
 func (self *Twitter) Mentions(options map[string] uint) (statuses []Status, err os.Error) {
-	const path = "/statuses/mentions.json";
+	const path = "/statuses/mentions.json"
 
-	return self.timeline(path, options, self.Username, self.Password, self.useSsl);
+	return self.timeline(path, options, self.Username, self.Password, self.useSsl)
 }
 
 // GetListsの引数マップキーに使用
 const (
-	OPTION_GetLists_Cursor = "?cursor=";
+	OPTION_GetLists_Cursor = "?cursor="
 )
 // GET lists
 //  user --- UserId or ScreenName
 func (self *Twitter) GetLists(user string, options map[string] int) (lists []List, err os.Error) {
-	path := fmt.Sprintf("/%s/lists.json", user);
+	path := fmt.Sprintf("/%s/lists.json", user)
 
 	// option parameters
 	if options != nil {
 		for opt, val := range options {
-			path += opt + strconv.Itoa(val);
+			path += opt + strconv.Itoa(val)
 		}
 	}
 
-	res, err := request(GET, HOST, path, "", self.Username, self.Password, self.useSsl);
+	res, err := request(GET, HOST, path, "", self.Username, self.Password, self.useSsl)
 	if res.StatusCode != 200 {
-		err = os.ErrorString(res.Status);
-		return;
+		err = os.ErrorString(res.Status)
+		return
 	}
 
 	// response body
 	if body, err := ioutil.ReadAll(res.Body); err == nil {
-		var l []List;
+		var l []List
 		if ok, errtok := json.Unmarshal(string(body), &l); ok == true {
-			lists = l;
+			lists = l
 		} else {
-			err = os.ErrorString(errtok);
+			err = os.ErrorString(errtok)
 		}
 	}
-	res.Body.Close();
+	res.Body.Close()
 
-	return;
+	return
 }
 
 // ListStatusesの引数マップキーに使用
 const (
-	OPTION_ListStatuses_SinceId = "?since_id=";
-	OPTION_ListStatuses_MaxId = "?max_id=";
-	OPTION_ListStatuses_PerPage = "?per_page=";
-	OPTION_ListStatuses_Page = "?page=";
+	OPTION_ListStatuses_SinceId = "?since_id="
+	OPTION_ListStatuses_MaxId = "?max_id="
+	OPTION_ListStatuses_PerPage = "?per_page="
+	OPTION_ListStatuses_Page = "?page="
 )
 // statuses mentions
 //  user --- UserId or ScreenName
 //  list --- ListId or ListName
 func (self *Twitter) ListStatuses(user, list string, options map[string] uint) (statuses []Status, err os.Error) {
-	path := fmt.Sprintf("/%s/lists/%s/statuses.json", user, list);
+	path := fmt.Sprintf("/%s/lists/%s/statuses.json", user, list)
 
-	return self.timeline(path, options, self.Username, self.Password, self.useSsl);
+	return self.timeline(path, options, self.Username, self.Password, self.useSsl)
 }
 
 // UsersSearchの引数マップキーに使用
 const (
-	OPTION_UsersSearch_PerPage = "?per_page=";
-	OPTION_UsersSearch_Page = "?page=";
+	OPTION_UsersSearch_PerPage = "?per_page="
+	OPTION_UsersSearch_Page = "?page="
 )
 // users search
 func (self *Twitter) UsersSearch(user string, options map[string] uint) (users []User, err os.Error) {
-	path := fmt.Sprintf("/1/users/search.json?q=%s", user);
+	path := fmt.Sprintf("/1/users/search.json?q=%s", user)
 
 	// option parameters
 	if options != nil {
 		for opt, val := range options {
-			path += opt + strconv.Uitoa(val);
+			path += opt + strconv.Uitoa(val)
 		}
 	}
 
-	res, err := request(GET, HOST, path, "", self.Username, self.Password, self.useSsl);
+	res, err := request(GET, HOST, path, "", self.Username, self.Password, self.useSsl)
 	if res.StatusCode != 200 {
-		err = os.ErrorString(res.Status);
-		return;
+		err = os.ErrorString(res.Status)
+		return
 	}
 
 	// response body
 	if body, err := ioutil.ReadAll(res.Body); err == nil {
-		var u []User;
+		var u []User
 		if ok, errtok := json.Unmarshal(string(body), &u); ok == true {
-			users = u;
+			users = u
 		} else {
-			err = os.ErrorString(errtok);
+			err = os.ErrorString(errtok)
 		}
 	}
-	res.Body.Close();
+	res.Body.Close()
 
-	return;
+	return
 }
 
 // 標準パッケージのhttpから持ってきた
 type readClose struct {
-	io.Reader;
-	io.Closer;
+	io.Reader
+	io.Closer
 }
 
 // http send
 // 標準パッケージのhttp.sendを元に改造した（ベーシック認証, SSL等に対応させる為）
 // Caller should close res.Body when done reading it.
 func send(req *http.Request) (res *http.Response, err os.Error) {
-	conn, err := net.Dial("tcp", "", req.URL.Host);
+	conn, err := net.Dial("tcp", "", req.URL.Host)
 	if err != nil {
-		return nil, err;
+		return nil, err
 	}
 
-	err = req.Write(conn);
+	err = req.Write(conn)
 	if err != nil {
-		conn.Close();
-		return nil, err;
+		conn.Close()
+		return nil, err
 	}
 
-	reader := bufio.NewReader(conn);
-	res, err = http.ReadResponse(reader);
+	reader := bufio.NewReader(conn)
+	res, err = http.ReadResponse(reader)
 	if err != nil {
-		conn.Close();
-		return nil, err;
+		conn.Close()
+		return nil, err
 	}
 
-	r := io.Reader(reader);
+	r := io.Reader(reader)
 	if v := res.GetHeader("Transfer-Encoding"); v == "chunked" {
 		// 現状はchunkedで返ってくることはないので、未サポートにしておく
-		conn.Close();
-		return nil, os.NewError("chunked is not implemented yet.");
+		conn.Close()
+		return nil, os.NewError("chunked is not implemented yet.")
 	} else if v := res.GetHeader("Content-Length"); v != "" {
-		n, err := strconv.Atoi64(v);
+		n, err := strconv.Atoi64(v)
 		if err != nil {
-			return nil, os.NewError("invalid Content-Length");
+			return nil, os.NewError("invalid Content-Length")
 		}
-		r = io.LimitReader(r, n);
+		r = io.LimitReader(r, n)
 	}
-	res.Body = readClose{r, conn};
+	res.Body = readClose{r, conn}
 
-	return;
+	return
 }
 
 // requestの引数methodに使用
 const (
-	GET = iota;
-	POST;
-	DELETE;
+	GET = iota
+	POST
+	DELETE
 )
-const HOST = "twitter.com";
+const HOST = "twitter.com"
 // http request
 // Caller should close res.Body when done reading it.
 func request(method int, host, path, body, user, pass string, useSsl bool) (res *http.Response, err os.Error) {
-	var req http.Request;
-	var url string;
+	var req http.Request
+	var url string
 
 	// url
 	if useSsl != true {
-		url = "http://" + host + ":80";
+		url = "http://" + host + ":80"
 	} else {
-		url = "https://" + host + ":443";
-		return nil, os.NewError("SSL is not implemented yet.");
+		url = "https://" + host + ":443"
+		return nil, os.NewError("SSL is not implemented yet.")
 	}
 
 	if req.URL, err = http.ParseURL(url + path); err != nil {
-		return nil, err;
+		return nil, err
 	}
 
 	// method
 	switch method {
 	case GET:
-		req.Method = "GET";
+		req.Method = "GET"
 	case POST:
-		req.Method = "POST";
+		req.Method = "POST"
 	case DELETE:
-		req.Method = "DELETE";
+		req.Method = "DELETE"
 	default:
-		return nil, os.NewError("invalid method");
+		return nil, os.NewError("invalid method")
 	}
 
 	// header
-	req.Header = make(map[string] string);
+	req.Header = make(map[string] string)
 
 	// authorization
 	if user != "" && pass != "" {
-		userpass := user + ":" + pass;
-		buf := make([]byte, base64.StdEncoding.EncodedLen(len(userpass)));
-		base64.StdEncoding.Encode(buf, strings.Bytes(userpass));
-		encodedUserpass := string(buf);
+		userpass := user + ":" + pass
+		buf := make([]byte, base64.StdEncoding.EncodedLen(len(userpass)))
+		base64.StdEncoding.Encode(buf, strings.Bytes(userpass))
+		encodedUserpass := string(buf)
 
-		req.Header["Authorization"] = "Basic " + encodedUserpass;
+		req.Header["Authorization"] = "Basic " + encodedUserpass
 	}
 
 	// content type
 	if method == POST {
-        req.Header["Content-Type"] = "application/x-www-form-urlencoded";
+        req.Header["Content-Type"] = "application/x-www-form-urlencoded"
 	}
 
 	// body
 	if body != "" {
-		req.Body = bytes.NewBufferString(body);
+		req.Body = bytes.NewBufferString(body)
 	}
 
-	return send(&req);
+	return send(&req)
 }
 
 // timeline取得
 func (self *Twitter) timeline(path string, options map[string] uint, user, pass string, useSsl bool) (statuses []Status, err os.Error) {
-	optpath := path;
+	optpath := path
 
 	// option parameters
 	if options != nil {
 		for opt, val := range options {
-			optpath += opt + strconv.Uitoa(val);
+			optpath += opt + strconv.Uitoa(val)
 		}
 	}
 
-	res, err := request(GET, HOST, optpath, "", user, pass, useSsl);
+	res, err := request(GET, HOST, optpath, "", user, pass, useSsl)
 	if res.StatusCode != 200 {
-		err = os.ErrorString(res.Status);
-		return;
+		err = os.ErrorString(res.Status)
+		return
 	}
 
 	// response body
 	if body, err := ioutil.ReadAll(res.Body); err == nil {
-		var s []Status;
+		var s []Status
 		if ok, errtok := json.Unmarshal(string(body), &s); ok == true {
 			// source
-			re, _ := regexp.Compile("<a[^>]*>(.*)</a>");
+			re, _ := regexp.Compile("<a[^>]*>(.*)</a>")
 			for i, sts := range s {
 				if srcs := re.MatchStrings(sts.Source); len(srcs) == 2 {
-					s[i].Source = srcs[1];
+					s[i].Source = srcs[1]
 				}
 			}
-			statuses = s;
+			statuses = s
 		} else {
-			err = os.ErrorString(errtok);
+			err = os.ErrorString(errtok)
 		}
 	}
-	res.Body.Close();
+	res.Body.Close()
 
-	return;
+	return
 }
